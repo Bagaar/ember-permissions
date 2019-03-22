ac6fbfb401fc47d92fa9939f5bab297ee4b97293
Bert De Block <bert.deblock@bagaar.be>
2019-03-22 10:09:18 +0100

    chore: Update linting setup (using prettier-standard)
    BREAKING CHANGE: prettier-standard requires node 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  plugins: [
-    'ember',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    '@bagaar/eslint-config-ember',
-  ],
+  plugins: ['ember'],
+  extends: ['plugin:ember/recommended', '@bagaar/eslint-config-ember'],
   env: {
-    browser: true,
+    browser: true
   },
   rules: {},
   overrides: [
@@ -27,26 +21,30 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'tests/dummy/config/**/*.js',
+        'tests/dummy/config/**/*.js'
       ],
       excludedFiles: [
         'addon/**',
         'addon-test-support/**',
         'app/**',
-        'tests/dummy/app/**',
+        'tests/dummy/app/**'
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015,
+        ecmaVersion: 2015
       },
       env: {
         browser: false,
-        node: true,
+        node: true
       },
       plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      }),
-    },
-  ],
-};
+      rules: Object.assign(
+        {},
+        require('eslint-plugin-node').configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
+    }
+  ]
+}
