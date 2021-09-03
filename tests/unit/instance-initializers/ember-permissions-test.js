8aa3615610b9ec8356cc48d614c1749044c553e9
Bert De Block <bert.deblock@bagaar.be>
2021-09-03 12:21:59 +0200

    test: update instance-initializer test

--- tests/unit/instance-initializers/ember-permissions-test.js
+++ tests/unit/instance-initializers/ember-permissions-test.js
@@ -1,28 +1,33 @@
-import PermissionsService from '@bagaar/ember-permissions/services/permissions';
 import Application from '@ember/application';
 import { run } from '@ember/runloop';
+import config from 'dummy/config/environment';
 import { initialize } from 'dummy/instance-initializers/ember-permissions';
+import Resolver from 'ember-resolver';
 import { module, test } from 'qunit';
 
 module('Unit | Instance Initializer | ember-permissions', function (hooks) {
   hooks.beforeEach(function () {
-    this.TestApplication = class TestApplication extends Application {};
+    this.TestApplication = class TestApplication extends Application {
+      modulePrefix = config.modulePrefix;
+      podModulePrefix = config.podModulePrefix;
+      Resolver = Resolver;
+    };
+
     this.TestApplication.instanceInitializer({
       name: 'initializer under test',
       initialize,
     });
+
     this.application = this.TestApplication.create({ autoboot: false });
     this.instance = this.application.buildInstance();
   });
 
   hooks.afterEach(function () {
-    run(this.application, 'destroy');
     run(this.instance, 'destroy');
+    run(this.application, 'destroy');
   });
 
   test('it works', async function (assert) {
-    this.instance.register('service:permissions', PermissionsService);
-
     await this.instance.boot();
 
     assert.ok(true);
