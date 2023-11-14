2aa6508ea117c4a035a156ba7b1bbaf6fda70863
Bert De Block <bert.deblock@bagaar.be>
2023-11-14 11:58:13 +0100

    chore: update Ember to v5.4

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,1 @@
-/**
- * Types for compiled templates.
- */
-declare module '@bagaar/ember-permissions/templates/*' {
-  import { TemplateFactory } from 'ember-cli-htmlbars';
-
-  const tmpl: TemplateFactory;
-
-  export default tmpl;
-}
+import '@glint/environment-ember-loose';
